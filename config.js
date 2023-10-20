fd0bc74754c3638c0d3ba86611a9868e00bb8fb3
Hazcodestuff <128824758+Hazcodestuff@users.noreply.github.com>
2023-10-20 23:42:27 +0800

    Create config.js

---
+++ config.js
@@ -0,0 +1,82 @@
+import { watchFile, unwatchFile } from 'fs'
+import chalk from 'chalk'
+import { fileURLToPath } from 'url'
+import fs from 'fs'
+import fetch from 'node-fetch'
+import axios from 'axios'
+
+
+global.owner = [
+  ['601112304952', 'Owner', true],
+  ['601154022158', 'Danker', true], 
+  [''] 
+] //Number of owners
+
+global.pairingNumber = "601154022158" //put your bot number here
+
+global.mods = ['601112304952'] 
+global.prems = ['601112304952']
+global.allowed = ['601112304952']
+global.keysZens = ['c2459db922', '37CC845916', '6fb0eff124']
+global.keysxxx = keysZens[Math.floor(keysZens.length * Math.random())]
+global.keysxteammm = ['29d4b59a4aa687ca', '5LTV57azwaid7dXfz5fzJu', 'cb15ed422c71a2fb', '5bd33b276d41d6b4', 'HIRO', 'kurrxd09', 'ebb6251cc00f9c63']
+global.keysxteam = keysxteammm[Math.floor(keysxteammm.length * Math.random())]
+global.keysneoxrrr = ['5VC9rvNx', 'cfALv5']
+global.keysneoxr = keysneoxrrr[Math.floor(keysneoxrrr.length * Math.random())]
+global.lolkeysapi = ['GataDios']
+
+global.APIs = { // API Prefix
+  // name: 'https://website'
+  xteam: 'https://api.xteam.xyz', 
+  dzx: 'https://api.dhamzxploit.my.id',
+  lol: 'https://api.lolhuman.xyz',
+  violetics: 'https://violetics.pw',
+  neoxr: 'https://api.neoxr.my.id',
+  zenzapis: 'https://zenzapis.xyz',
+  akuari: 'https://api.akuari.my.id',
+  akuari2: 'https://apimu.my.id',
+  nrtm: 'https://fg-nrtm.ddns.net',
+  bg: 'http://bochil.ddns.net',
+  fgmods: 'https://api-fgmods.ddns.net'
+}
+global.APIKeys = { // APIKey Here
+  // 'https://website': 'apikey'
+  'https://api.xteam.xyz': 'd90a9e986e18778b',
+  'https://api.lolhuman.xyz': '85faf717d0545d14074659ad',
+  'https://api.neoxr.my.id': `${keysneoxr}`,	
+  'https://violetics.pw': 'beta',
+  'https://zenzapis.xyz': `${keysxxx}`, 
+  'https://api-fgmods.ddns.net': 'fg-dylux'
+}
+
+// Sticker WM
+global.botname = 'Dank Memer'
+global.premium = 'false'
+global.packname = 'Memes' 
+global.author = 'The Meme Community' 
+global.menuvid = 'https://telegra.ph/file/b22bf462e81e098f85197.jpg'
+global.igfg = '' 
+global.dygp = 'https://chat.whatsapp.com/L8g84KJhdcoFAi6QG21hOY'
+global.fgsc = 'https://github.com/Guru322/GURU-BOT' 
+global.fgyt = ''
+global.fgpyp = ''
+global.fglog = 'https://raw.githubusercontent.com/Guru322/api/Guru/guru.jpg' 
+global.thumb = fs.readFileSync('./Assets/Gurulogo.jpg')
+
+
+global.wait = '*⌛ _Loading..._'
+global.rwait = '⌛'
+global.dmoji = '🤭'
+global.done = '✅'
+global.error = '❌' 
+global.xmoji = '🔥' 
+
+global.multiplier = 69 
+global.maxwarn = '3' // máxima advertencias
+
+let file = fileURLToPath(import.meta.url)
+watchFile(file, () => {
+  unwatchFile(file)
+  console.log(chalk.redBright("Update 'config.js'"))
+  import(`${file}?update=${Date.now()}`)
+})
